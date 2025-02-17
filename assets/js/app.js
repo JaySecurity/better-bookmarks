a25e95b8d828b0ed7910a52e4a4daff9217a07cf
Jason Nicholls <jasonnicholls1981@gmail.com>
2025-02-17 15:30:27 -0500

    Setup node inspection, data fetching

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,18 @@
   // src/app.ts
   var text = document.getElementById("search");
   var search = document.getElementById("search-btn");
+  var userTags;
+  var tagsMap;
+  var folders;
+  fetchData();
   if (!text || !search) {
     console.error("Error loading page elements");
   }
   search.addEventListener("click", () => {
     chrome.bookmarks.getTree((res) => {
-      const tree = res[0].children[0].children;
-      if (tree && tree?.length > 0) {
+      console.log(userTags, tagsMap);
+      const tree = res[0]?.children?.[0]?.children;
+      if (tree && tree.length > 0) {
         for (const node of tree) {
           console.log(node?.title == "" ? "Bookmarks" : `${node?.title} - ${node.id}`);
         }
@@ -17,5 +22,38 @@
     });
   });
   chrome.runtime.sendMessage("Test Meaasge");
+  async function fetchData() {
+    const data = await Promise.all([
+      chrome.storage.sync.get("userTags"),
+      chrome.storage.sync.get("folders"),
+      chrome.storage.sync.get("tagsMap")
+    ]);
+    userTags = data[0].userTags || {};
+    folders = data[1].folders;
+    tagsMap = data[1].tagsMap || {};
+    if (!folders || !tagsMap) {
+      const tree = await chrome.bookmarks.getTree();
+      const bookmarks = tree[0]?.children?.[0]?.children;
+      if (!bookmarks) {
+        return;
+      }
+      folders = /* @__PURE__ */ new Set();
+      for (const node of bookmarks) {
+        inspectNode(node);
+      }
+    }
+    console.log(folders);
+    console.log(tagsMap);
+  }
+  function inspectNode(node) {
+    if (node.hasOwnProperty("children")) {
+      folders.add(node.title);
+      for (const child of node?.children) {
+        inspectNode(child);
+      }
+    } else {
+      tagsMap[node.id] = [];
+    }
+  }
 })();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-
-
 const text = document.getElementById('search') as HTMLInputElement;
 const search = document.getElementById('search-btn') as HTMLButtonElement;
+
+// fetch from storage - User Tags, Id => tags map
+let userTags, tagsMap, folders
+fetchData()
 
 
 if (!text || !search) {
@@ -11,8 +13,10 @@ if (!text || !search) {
 
 search.addEventListener('click', () => {
   chrome.bookmarks.getTree(res => {
-    const tree = res[0]?.children[0]?.children
-    if (tree && tree?.length > 0) {
+    // Fetch Main Bookmarks Node
+    console.log(userTags, tagsMap)
+    const tree = res[0]?.children?.[0]?.children
+    if (tree && tree.length > 0) {
       for (const node of tree) {
         console.log(node?.title == "" ? "Bookmarks" : `${node?.title} - ${node.id}`)
       }
@@ -21,3 +25,40 @@ search.addEventListener('click', () => {
 });
 
 chrome.runtime.sendMessage("Test Meaasge")
+
+
+async function fetchData() {
+  const data = await Promise.all([
+    chrome.storage.sync.get('userTags'),
+    chrome.storage.sync.get('folders'),
+    chrome.storage.sync.get('tagsMap')
+  ])
+  userTags = data[0].userTags || {}
+  folders = data[1].folders
+  tagsMap = data[1].tagsMap || {}
+  // create folders array if not found
+  if (!folders || !tagsMap) {
+    const tree = await chrome.bookmarks.getTree()
+    const bookmarks = tree[0]?.children?.[0]?.children
+    if (!bookmarks) {
+      return
+    }
+    folders = new Set()
+    for (const node of bookmarks) {
+      inspectNode(node)
+    }
+  }
+  console.log(folders)
+  console.log(tagsMap)
+}
+
+function inspectNode(node: chrome.bookmarks.BookmarkTreeNode) {
+  if (node.hasOwnProperty('children')) {
+    folders.add(node.title)
+    for (const child of node?.children) {
+      inspectNode(child)
+    }
+  } else {
+    tagsMap[node.id] = []
+  }
+}
